5475d800b1079e57523b052a9f2cb78c275a521c
Stephen Muchendu <muchendumaina66@gmail.com>
2023-01-23 20:32:54 +0300

    added stateProvider file

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
 import App from './App';
+import StateProvider from 'context';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <StateProvider>
+      <App />
+    </StateProvider>
   </React.StrictMode>
 );
