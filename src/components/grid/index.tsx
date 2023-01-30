db9ab7ed2a8df147f2a474a2f52e8557a813b2b5
Stephen Muchendu <muchendumaina66@gmail.com>
2023-01-30 23:05:14 +0300

    added bugs

--- src/components/grid/index.tsx
+++ src/components/grid/index.tsx
@@ -1,76 +1,90 @@
 import { useEffect, useState } from 'react';
 
 import { useApp } from 'context';
-// import Subgrid from 'components/subgrid';
+import { Input } from 'components';
 import styles from './styles.module.scss';
 
 export default function Grid() {
-  const { generateSudoku, solveSudoku, isSolving } = useApp();
+  const { generateSudoku, solveSudoku, grid, setGrid } = useApp();
 
-  const [grid, setGrid] = useState<number[][]>();
-  const [isSolved, setIsSolved] = useState(false);
+  const [solve, setSolve] = useState<boolean>(false);
+  // const [isSolved, setIsSolved] = useState(false);
 
   useEffect(() => {
-    setGrid(generateSudoku().sudoku);
+    generateSudoku();
   }, []);
 
-  const handleSolve = () => {
-    if (grid) {
+  useEffect(() => {
+    if (solve) {
+      if (!grid) return;
       const solvedGrid = solveSudoku(grid)!;
       setGrid(solvedGrid);
-      setIsSolved(true);
+      console.log(solvedGrid);
     }
-    console.log(isSolved);
+  }, [solve]);
+
+  const handleSolve = () => {
+    setSolve(true);
   };
 
-  const getGridClass = (i: number, j: number) => {
-    if (i < 3 && j < 3) {
-      return 'top-left-grid';
-    } else if (i < 3 && j > 2 && j < 6) {
-      return 'top-center-grid';
-    } else if (i < 3 && j > 5) {
-      return 'top-right-grid';
-    } else if (i > 2 && i < 6 && j < 3) {
-      return 'mid-left-grid';
-    } else if (i > 2 && i < 6 && j > 2 && j < 6) {
-      return 'mid-center-grid';
-    } else if (i > 2 && i < 6 && j > 5) {
-      return 'mid-right-grid';
-    } else if (i > 5 && j < 3) {
-      return 'bottom-left-grid';
-    } else if (i > 5 && j > 2 && j < 6) {
-      return 'bottom-center-grid';
-    } else if (i > 5 && j > 5) {
-      return 'bottom-right-grid';
-    } else {
-      return 'box';
-    }
+  const handleNewGrid = () => {
+    generateSudoku();
+    // setIsSolved(false);
   };
+  //   if (i < 3 && j < 3) {
+  //     return 'top-left-grid';
+  //   } else if (i < 3 && j > 2 && j < 6) {
+  //     return 'top-center-grid';
+  //   } else if (i < 3 && j > 5) {
+  //     return 'top-right-grid';
+  //   } else if (i > 2 && i < 6 && j < 3) {
+  //     return 'mid-left-grid';
+  //   } else if (i > 2 && i < 6 && j > 2 && j < 6) {
+  //     return 'mid-center-grid';
+  //   } else if (i > 2 && i < 6 && j > 5) {
+  //     return 'mid-right-grid';
+  //   } else if (i > 5 && j < 3) {
+  //     return 'bottom-left-grid';
+  //   } else if (i > 5 && j > 2 && j < 6) {
+  //     return 'bottom-center-grid';
+  //   } else if (i > 5 && j > 5) {
+  //     return 'bottom-right-grid';
+  //   } else {
+  //     return 'box';
+  //   }
+  // };
 
   return (
     <div className={styles.container}>
-      <table className={styles.main}>
-        <tbody>
-          {grid?.map((row, i) => (
-            <tr key={i}>
-              {row.map((cell, j) => (
-                <td key={j} className={styles[getGridClass(i, j)]}>
-                  {cell}
-                </td>
-              ))}
-            </tr>
-          ))}
-        </tbody>
-      </table>
-      {isSolving ? (
-        <h4>Solving.....</h4>
-      ) : isSolved ? (
-        <button>Get New Grid</button>
-      ) : (
-        <button className={styles.button} onClick={() => handleSolve()}>
-          Solve
-        </button>
-      )}
+      <div className='App'>
+        <table>
+          <tbody>
+            {grid?.map((row, i) => (
+              <tr key={i}>
+                {row.map((i: number, j: number) => (
+                  <td key={j}>
+                    <Input i={i} />
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <button onClick={() => handleNewGrid()}>Get New Grid</button>
+      <button onClick={() => handleSolve()}>Solve</button>
     </div>
   );
 }
+
+// [
+//   [0, 0, 0, 0, 0, 0, 0, 0, 0],
+//   [0, 0, 0, 0, 1, 0, 0, 0, 0],
+//   [0, 0, 7, 4, 0, 0, 0, 0, 0],
+//   [0, 0, 0, 0, 0, 0, 0, 0, 0],
+//   [0, 0, 0, 0, 0, 0, 0, 0, 0],
+//   [0, 0, 0, 0, 0, 0, 8, 0, 0],
+//   [0, 0, 0, 0, 0, 0, 0, 0, 0],
+//   [0, 3, 0, 0, 0, 0, 0, 0, 0],
+//   [0, 0, 0, 0, 0, 0, 0, 0, 0]
+// ];
