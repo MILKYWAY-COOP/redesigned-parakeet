4a174ff5759a72090973eabb87d6524b763aa31d
Stephen Muchendu <muchendumaina66@gmail.com>
2023-01-31 19:15:28 +0300

    added handleNewGrid() function

--- src/components/grid/index.tsx
+++ src/components/grid/index.tsx
@@ -1,5 +1,3 @@
-import { useEffect, useState } from 'react';
-
 import { useApp } from 'context';
 import { Input } from 'components';
 import styles from './styles.module.scss';
@@ -7,52 +5,17 @@ import styles from './styles.module.scss';
 export default function Grid() {
   const { generateSudoku, solveSudoku, grid, setGrid } = useApp();
 
-  const [solve, setSolve] = useState<boolean>(false);
-  // const [isSolved, setIsSolved] = useState(false);
-
-  useEffect(() => {
-    generateSudoku();
-  }, []);
-
-  useEffect(() => {
-    if (solve) {
-      if (!grid) return;
-      const solvedGrid = solveSudoku(grid)!;
-      setGrid(solvedGrid);
-      console.log(solvedGrid);
-    }
-  }, [solve]);
+  const handleNewGrid = () => {
+    const { sudoku } = generateSudoku();
+    // setGrid((prev) => (prev = sudoku));
+    setGrid(sudoku);
+  };
 
   const handleSolve = () => {
-    setSolve(true);
+    const { board } = solveSudoku(grid)!;
+    console.log(board);
+    setGrid(board);
   };
-
-  const handleNewGrid = () => {
-    generateSudoku();
-    // setIsSolved(false);
-  };
-  //   if (i < 3 && j < 3) {
-  //     return 'top-left-grid';
-  //   } else if (i < 3 && j > 2 && j < 6) {
-  //     return 'top-center-grid';
-  //   } else if (i < 3 && j > 5) {
-  //     return 'top-right-grid';
-  //   } else if (i > 2 && i < 6 && j < 3) {
-  //     return 'mid-left-grid';
-  //   } else if (i > 2 && i < 6 && j > 2 && j < 6) {
-  //     return 'mid-center-grid';
-  //   } else if (i > 2 && i < 6 && j > 5) {
-  //     return 'mid-right-grid';
-  //   } else if (i > 5 && j < 3) {
-  //     return 'bottom-left-grid';
-  //   } else if (i > 5 && j > 2 && j < 6) {
-  //     return 'bottom-center-grid';
-  //   } else if (i > 5 && j > 5) {
-  //     return 'bottom-right-grid';
-  //   } else {
-  //     return 'box';
-  //   }
-  // };
 
   return (
     <div className={styles.container}>
